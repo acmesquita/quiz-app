723a1650f617e0fd05b0d118d7f55d9b38c4e732
Catharina Mesquita <catha.ana.1994@gmail.com>
2020-04-05 15:47:04 -0300

    Adicionando RestClient para realizar as requisições na api

--- app/services/find_questions_api.rb
+++ app/services/find_questions_api.rb
@@ -3,6 +3,9 @@ class FindQuestionsApi
   URL = 'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple'
   
   def call
+    respose = RestClient.get(URL)
     
+    body = JSON.parse(respose.body)
+    puts body.results
   end
 end

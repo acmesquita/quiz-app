3f4542d2da742af1c2afe6d2583d780d6e79ec4f
Catharina Mesquita <catha.ana.1994@gmail.com>
2020-04-10 17:43:43 -0300

    Adicionando um call static  para melhorar a chamada no controller

--- app/services/find_questions_api.rb
+++ app/services/find_questions_api.rb
@@ -1,6 +1,10 @@
 class FindQuestionsApi
 
   URL = 'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple'
+
+  def self.call
+    new.call
+  end
   
   def call
     request.map do |q|

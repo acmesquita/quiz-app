049d7335c700076a12a973cfb39f87b8a05fa618
Catharina Mesquita <catha.ana.1994@gmail.com>
2020-04-10 17:45:06 -0300

    Implementando testes para o index do controller

--- spec/controllers/game_controller_spec.rb
+++ spec/controllers/game_controller_spec.rb
@@ -7,6 +7,34 @@ RSpec.describe GameController, type: :controller do
       get :index
       expect(response).to have_http_status(:success)
     end
+
+    it "returns instance of game" do
+      get :index
+      expect(assigns(:game)).not_to be_nil
+    end
+
+    it "returns true game#questions not empty" do
+      mock = JSON.parse('{
+        "results": [
+          {
+            "category": "General Knowledge",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "Which company did Valve cooperate with in the creation of the Vive?",
+            "correct_answer": "HTC",
+            "incorrect_answers": [
+              "Oculus",
+              "Google",
+              "Razer"
+            ]
+          }
+        ]
+      }')['results']
+      allow_any_instance_of(FindQuestionsApi).to receive(:request).and_return(mock)
+      get :index
+      
+      expect(assigns(:game).questions.size).to eql(1)
+    end
   end
 
 end

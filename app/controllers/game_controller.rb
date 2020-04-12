6ac0b3bdf70121e1878fb75b9be8e2444ef6e3d8
Catharina Mesquita <catha.ana.1994@gmail.com>
2020-04-12 11:37:32 -0300

    Criando lógica de validação da resposta

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -3,4 +3,17 @@ class GameController < ApplicationController
     questions = FindQuestionsApi.call
     @game = Game.new(questions, 'Catharina')
   end
+
+  def validade_answer
+    if (@game.question_current.correct_answer == answer_user){
+      @game.question_current = @game.questions[@game.question_position + 1] rescue nil
+    }
+    render @game
+  end
+
+  private
+
+  def answer_user
+    params.require(:answer).permit!
+  end
 end
